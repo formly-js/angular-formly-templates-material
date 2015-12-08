606b2e8f61ac8a34870c5e8bb3783383b3935c82
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2015-12-08 21:58:09 +0100

    chore: update repo after transfer to formly-js

--- package.js
+++ package.js
@@ -6,7 +6,7 @@ Package.describe({
     version: "0.7.1",
 
     documentation: 'README.md',
-    git: 'https://github.com/wieldo/angular-formly-templates-material.git'
+    git: 'https://github.com/formly-js/angular-formly-templates-material.git'
 });
 
 Package.onUse(function (api) {
