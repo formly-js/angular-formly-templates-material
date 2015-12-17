3a861d0b258fbfc16191cd9e2613e56547908a98
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2015-12-18 00:09:43 +0100

    test(chips): onAdd callback

--- tests/types/chips-spec.js
+++ tests/types/chips-spec.js
@@ -2,74 +2,86 @@ import testUtils from './../test-utils';
 
 describe("formlyMaterial - chips type", () => {
 
-    //
-    // vars
-    //
-    let formlyConfig;
-    let $compile;
-    let $rootScope;
-    let $scope;
-    let element;
-    let field;
-    //
-    // helpers
-    //
+  //
+  // vars
+  //
+  let formlyConfig;
+  let $compile;
+  let $rootScope;
+  let $scope;
+  let element;
+  let field;
 
-    function compile(options) {
-        $scope = $rootScope.$new();
-        $scope.fields = [angular.merge({}, {
-            key: 'testField',
-            type: 'chips',
-            templateOptions: {
-                label: 'test field',
-                placeholder: "+tags",
-                secondaryPlaceholder: "Add tag",
-                deleteButtonLabel: "Remove",
-                deleteHint: "Remove tag"
-            }
-        }, options)];
+  function onAdd() {
+    return true;
+  }
+  //
+  // helpers
+  //
 
-        let form = $compile(testUtils.getFormTemplate())($scope);
-        
-        $scope.$digest();
-        field = $scope.fields[0];
-        element = form.find('[ng-model]');
-    }
+  function compile(options) {
+    $scope = $rootScope.$new();
+    $scope.fields = [angular.merge({}, {
+      key: 'testField',
+      type: 'chips',
+      templateOptions: {
+        label: 'test field',
+        placeholder: "+tags",
+        secondaryPlaceholder: "Add tag",
+        deleteButtonLabel: "Remove",
+        deleteHint: "Remove tag",
+        onAdd: onAdd
+      }
+    }, options)];
 
-    //
-    // tests
-    //
+    let form = $compile(testUtils.getFormTemplate())($scope);
 
-    beforeEach(() => {
-        window.module('formlyMaterial');
+    $scope.$digest();
+    field = $scope.fields[0];
+    element = form.find('[ng-model]');
+  }
 
-        inject((_$compile_, _$rootScope_, _formlyConfig_) => {
-            $compile = _$compile_;
-            $rootScope = _$rootScope_;
-            formlyConfig = _formlyConfig_;
-        });
+  //
+  // tests
+  //
 
-        compile();
+  beforeEach(() => {
+    window.module('formlyMaterial');
+
+    inject((_$compile_, _$rootScope_, _formlyConfig_) => {
+      $compile = _$compile_;
+      $rootScope = _$rootScope_;
+      formlyConfig = _formlyConfig_;
     });
 
-    it('should be md-chips element', () => {
-        expect(element[0].nodeName).toBe('MD-CHIPS');
-    });
+    compile();
+  });
 
-    it('should have placeholder', () => {
-        expect(element.attr('placeholder')).toBe(field.templateOptions.placeholder);
-    });
+  it('should be md-chips element', () => {
+    expect(element[0].nodeName).toBe('MD-CHIPS');
+  });
 
-    it('should have secondary placeholder', () => {
-        expect(element.attr('secondary-placeholder')).toBe(field.templateOptions.secondaryPlaceholder);
-    });
+  it('should have placeholder', () => {
+    expect(element.attr('placeholder')).toBe(field.templateOptions.placeholder);
+  });
 
-    it('should have delete button label', () => {
-        expect(element.attr('delete-button-label')).toBe(field.templateOptions.deleteButtonLabel);
-    });
+  it('should have secondary placeholder', () => {
+    expect(element.attr('secondary-placeholder')).toBe(field.templateOptions.secondaryPlaceholder);
+  });
 
-    it('should have delete hint', () => {
-        expect(element.attr('delete-hint')).toBe(field.templateOptions.deleteHint);
-    });
+  it('should have delete button label', () => {
+    expect(element.attr('delete-button-label')).toBe(field.templateOptions.deleteButtonLabel);
+  });
+
+  it('should have delete hint', () => {
+    expect(element.attr('delete-hint')).toBe(field.templateOptions.deleteHint);
+  });
+
+  it('should have onAdd callback', () => {
+    const scope = angular.element(element).scope();
+
+    expect(element.attr('md-on-add')).toBe("options.templateOptions['onAdd']");
+    expect(scope.options.templateOptions.onAdd).toBe(onAdd);
+  });
 
 });
