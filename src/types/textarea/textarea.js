5de28b48dc609be3fc68b61701e0484ea1d8d9fe
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2015-12-17 20:35:10 +0100

    feat(textarea): api check for grow and default true

--- src/types/textarea/textarea.js
+++ src/types/textarea/textarea.js
@@ -9,12 +9,16 @@ export default (formlyConfigProvider) => {
             ngModelAttrs: {
                 rows: {attribute: 'rows'},
                 cols: {attribute: 'cols'}
+            },
+            templateOptions: {
+              grow: true
             }
         },
         apiCheck: (check) => ({
             templateOptions: {
                 rows: check.number.optional,
-                cols: check.number.optional
+                cols: check.number.optional,
+                grow: check.bool.optional
             }
         })
     });
