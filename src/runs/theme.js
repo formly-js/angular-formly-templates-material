6cafd0ed7472a94730338173988d5b7fa112c4c2
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2015-12-18 14:01:18 +0100

    style(runs): lint error free

--- src/runs/theme.js
+++ src/runs/theme.js
@@ -5,6 +5,8 @@ from './../helpers/index.js';
 
 export default (formlyConfigProvider) => {
   formlyConfigProvider.templateManipulators.preWrapper.push((template, options) => {
+    // adds md-theme only when:
+    // templateOptions.theme is defined
     if (angular.isDefined(options.templateOptions.theme)) {
       return ngModelAttrsManipulator(template, options, 'md-theme', options.templateOptions.theme);
     }
