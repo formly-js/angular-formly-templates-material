2dacfe17ece212da0f13dae20440144a3ad301a9
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2015-12-29 09:34:31 +0100

    refactor(theme): typeof instead of angular helper

--- src/runs/theme.js
+++ src/runs/theme.js
@@ -7,7 +7,7 @@ export default (formlyConfigProvider) => {
   formlyConfigProvider.templateManipulators.preWrapper.push((template, options) => {
     // adds md-theme only when:
     // templateOptions.theme is defined
-    if (angular.isDefined(options.templateOptions.theme)) {
+    if (typeof options.templateOptions.theme !== 'undefined') {
       return ngModelAttrsManipulator(template, options, 'md-theme', options.templateOptions.theme);
     }
     return template;
