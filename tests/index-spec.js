dcf942f6e0c2e5f20b60484a6f91a2aa72c9c2da
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2015-12-29 23:51:42 +0100

    refactor: remove mocking console.warn

--- tests/index-spec.js
+++ tests/index-spec.js
@@ -4,6 +4,3 @@ import './helpers';
 import './runs';
 import './types';
 import './wrappers';
-
-// mock console warn (we don't need no education - from formly ;] )
-console.warn = function consoleWarn() {};
