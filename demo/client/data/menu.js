299a14b7bf3809e8454918f5ef1f05f0fa895acf
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-01-14 20:05:17 +0100

    demo: add documentation of Menu

--- demo/client/data/menu.js
+++ demo/client/data/menu.js
@@ -17,6 +17,12 @@ class Menu {
     this.addHeading('advanced');
   }
 
+  /**
+   * Add new menu item
+   * @param {String} name  Link's label
+   * @param {String} type  Can be heading, toggle or link
+   * @param {String|undefined} id  Used in /demo/:id
+   */
   add(name, type, id) {
     if (typeof name !== 'string' || name.length === 0) {
       throw new Error(`[Menu] name has to be a string`);
@@ -33,18 +39,37 @@ class Menu {
     });
   }
 
+  /**
+   * Shorthand method to add menu-link
+   * @param {String} name
+   * @param {String} id
+   */
   addLink(name, id) {
     this.add(name, 'link', id || name);
   }
 
+  /**
+   * Shorthand method to add menu-heading
+   * @param {String} name
+   */
   addHeading(name) {
     this.add(name, 'heading');
   }
 
+  /**
+   * Shorthand method to add menu-toggle
+   * @param {String} name
+   */
   addToggle(name) {
     this.add(name, 'toggle');
   }
 
+  /**
+   * Add a child to menu-toggle
+   * @param {String} parent  Parent menu item id
+   * @param {String} name  item's label
+   * @param {String} id item's identifier used as value of id parameter
+   */
   addChild(parent, name, id) {
     const found = _.find(this.menu, (item) => item.name === parent);
 
@@ -64,10 +89,10 @@ class Menu {
     });
   }
 
-  find(id) {
-    
-  }
-
+  /**
+   * Get menu
+   * @return {Object}
+   */
   get() {
     return {
       sections: this.menu
