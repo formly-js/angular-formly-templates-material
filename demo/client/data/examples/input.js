112119dd8508dadb90901a5af901ee286dd0f491
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-01-19 19:36:40 +0100

    demo(input): add validation and more options

--- demo/client/data/examples/input.js
+++ demo/client/data/examples/input.js
@@ -1,15 +1,44 @@
 const { SetModule } = angular2now;
 
 SetModule('demo').run(['Examples', 'Menu', (Examples, Menu) => {
+  const fields = [];
+
+  fields.push({
+    key: 'name',
+    type: 'input',
+    templateOptions: {
+      label: 'Name',
+      pattern: '[a-zA-Z]+'
+    },
+    validation: {
+      messages: {
+        pattern: () => 'a-zA-Z only'
+      }
+    }
+  });
+
+  fields.push({
+    key: 'sum',
+    type: 'input',
+    templateOptions: {
+      type: 'number',
+      label: 'Sum',
+      step: 0.5,
+      min: 0,
+      max: 10
+    },
+    validation: {
+      messages: {
+        max: () => '10 is max',
+        min: () => '0 is min',
+        number: () => 'Invalid format'
+      }
+    }
+  });
+
   // set example
   Examples.set('input', {
-    fields: [{
-      key: 'text',
-      type: 'input',
-      templateOptions: {
-        label: 'Text'
-      }
-    }]
+    fields
   }, 'types/input.md');
 
   // add menu item to types
