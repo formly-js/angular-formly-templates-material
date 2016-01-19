a1322322857c2f53c37df012a59e40f3e2cdbf2a
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-01-19 19:55:35 +0100

    demo(textarea): add grow option

--- demo/client/data/examples/textarea.js
+++ demo/client/data/examples/textarea.js
@@ -1,15 +1,28 @@
 const { SetModule } = angular2now;
 
 SetModule('demo').run(['Examples', 'Menu', (Examples, Menu) => {
+  const fields = [];
+
+  fields.push({
+    key: 'text-grow',
+    type: 'textarea',
+    templateOptions: {
+      label: 'Growing textarea'
+    }
+  });
+
+  fields.push({
+    key: 'text-notgrow',
+    type: 'textarea',
+    templateOptions: {
+      label: 'Not growing textarea',
+      grow: false
+    }
+  });
+
   // set example
   Examples.set('textarea', {
-    fields: [{
-      key: 'text',
-      type: 'textarea',
-      templateOptions: {
-        label: 'Text'
-      }
-    }]
+    fields
   }, 'types/textarea.md');
 
   // add menu item to types
